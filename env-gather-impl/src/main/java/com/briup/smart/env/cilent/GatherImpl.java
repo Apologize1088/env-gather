ba33ca949c658dd1d62f900f6be32c288de31b77
Apologize1088 <2587802491@qq.com>
2022-10-31 16:36:09 +0800

    完成类型查找

--- env-gather-impl/src/main/java/com/briup/smart/env/cilent/GatherImpl.java
+++ env-gather-impl/src/main/java/com/briup/smart/env/cilent/GatherImpl.java
@@ -3,6 +3,7 @@ package com.briup.smart.env.cilent;
 import com.briup.smart.env.client.Gather;
 import com.briup.smart.env.entity.Environment;
 
+import java.awt.*;
 import java.io.*;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -11,21 +12,26 @@ import java.util.List;
 
 public class GatherImpl implements Gather {
     @Override
+
     public Collection<Environment> gather() throws Exception {
         List<Environment> list = new ArrayList<>();
-        File file = new File("");
+        File file = new File("C:\\Users\\Administrator\\Desktop\\data-file-simple");
+        if (file==null){
+            return null;
+        }
         BufferedReader br = null;
 
         br = new BufferedReader(new FileReader(file));
-        String countent = "";
-        while (!"".equals(countent = br.readLine())){
-            String[] split = countent.split("|");
+        String countent = null;
+        while ((countent = br.readLine())!=null){
+            String[] split = countent.split("\\|");
             String[] names = getName(split[3]);
             for (String name : names) {
                 Environment environment = processingData(name, split);
                 list.add(environment);
             }
         }
+        br.close();
         return list;
     }
 
@@ -41,24 +47,63 @@ public class GatherImpl implements Gather {
         String desId = split[1];
         String devId = split[2];
         String sersorAddress = split[3];
-        int count = Integer.getInteger(split[4]);
+        int count = Integer.valueOf(split[4]);
         String cmd = split[5];
-        int status  = Integer.getInteger(split[7]);
+        int status  = Integer.valueOf(split[7]);
         float data = getData(name,split[6]);
         Timestamp gatherData = getDate(split[8]);
         return new Environment(name,srcId,desId,devId,sersorAddress,count,cmd,status,data,gatherData);
     }
 
+    /**
+     * 获取时间
+     * @param s
+     * @return
+     */
     private Timestamp getDate(String s) {
-        String getDate = s;
-        return Timestamp.valueOf(getDate);
+        long time = Long.valueOf(s);
+        return new Timestamp(time);
     }
 
 
-    private float getData(String sersorAddress,String data){
+    /**
+     * 环境数据处理
+     * @param name
+     * @param data
+     * @return
+     */
+    private float getData(String name,String data){
+        float concreteData = 0;
+        char[] dataChars = data.toCharArray();
+        if ("温度".equals(name)){
+            String temperature = new String(dataChars, 0, 4);
+            float i = Integer.parseInt(temperature,16);
+            concreteData = (i * (0.00268127F))-46.85F;
+        }
+        if ("湿度".equals(name)){
+            String humidity = new String(dataChars, dataChars.length/2-1, 4);
+            float i = Integer.parseInt(humidity,16);
+            concreteData = (i*0.00190735F)-6;
 
+        }
+        if ("光照强度".equals(name)){
+            String illumination = new String(dataChars, 0, 4);
+            concreteData = Integer.parseInt(illumination,16);
+
+        }
+        if ("二氧化碳".equals(name)){
+            String carbonDioxide  = new String(dataChars, 0, 4);
+            concreteData = Integer.parseInt(carbonDioxide,16);
+
+        }
+        return concreteData;
     }
 
+    /**
+     * 获取环境名称
+     * @param addressOfSensor
+     * @return
+     */
     private String[] getName(String addressOfSensor){
         String[] name = null;
         if ("16".equals(addressOfSensor)){
@@ -68,9 +113,8 @@ public class GatherImpl implements Gather {
             name = new String[]{"光照强度"};
         }
         if ("1280".equals(addressOfSensor)){
-            name = new String[]{"光照强度"};
+            name = new String[]{"二氧化碳"};
         }
         return name;
     }
-
 }
---
+++ env-gather-impl/src/main/java/com/briup/smart/env/test/GatherImplTest.java
@@ -0,0 +1,19 @@
+package com.briup.smart.env.test;
+
+import com.briup.smart.env.cilent.GatherImpl;
+import com.briup.smart.env.entity.Environment;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+public class GatherImplTest {
+    @Test
+    public void gatherTest(){
+        try {
+            Collection<Environment> gather = new GatherImpl().gather();
+            System.out.println(gather);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
